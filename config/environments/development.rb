4cb59408dd8b0ab1393f3a5afa458ba94d16e1b0
Amit Kumar <amitkchauhan1508@gmail.com>
2014-03-20 01:24:01 +0530

    Added sass support -4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,6 +21,9 @@ Bootcamp::Application.configure do
 
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
+ 
+ # Do not compress assets
+  config.assets.compress = false
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
